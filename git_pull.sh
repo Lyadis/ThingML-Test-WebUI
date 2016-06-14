5510bedaed5514e39730fd9180f76dd72dc45d08
sintef <nicolas.harrand@gmail.com>
2016-06-14 17:05:40 +0200

    ongoingwork thingml ws server

--- git_pull.sh
+++ git_pull.sh
@@ -5,17 +5,18 @@ testjar_dir="$thingml_dir/testJar"
 log_dir="/thingml/web"
 log_file="$log_dir/git_pull_log"
 
-echo "Start" > $log_file
-echo "Start"
+echo "> Start"
+echo "> mv to thingml dir"
 cd $thingml_dir
-echo "> git pull" >> $log_file
+echo "> save old config"
+mv $testjar_dir/config.properties $testjar_dir/old.properties
 echo "> git pull"
-git pull >> $log_file 2>&1
-echo "> mvn clean install" >> $log_file
+git pull
+echo "> restore old config"
+mv $testjar_dir/old.properties $testjar_dir/config.properties
 echo "> mvn clean install"
 mvn clean install 2>> $log_file
 cd $testjar_dir
-echo "> mvn clean install" >> $log_file
 echo "> mvn clean install"
 mvn clean install 2>> $log_file
-echo "> DONE" >> $log_file
+echo "> DONE"
--- run_tests.sh
+++ run_tests.sh
@@ -5,23 +5,18 @@ testjar_dir="$thingml_dir/testJar"
 log_dir="/thingml/web"
 log_file="$log_dir/run_tests_log"
 
-echo "Start"
-echo "Start" > $log_file
+echo "> Start"
 cd $testjar_dir
 
 echo "> rm -r m*_testDir"
-echo "> rm -r m*_testDir" >> $log_file
-rm -r m*_testDir >> $log_file 2>&1
+rm -r m*_testDir
 
 echo "> java -cp .:target/testJar-0.7.0-SNAPSHOT-jar-with-dependencies.jar org.thingml.loadbalancer.LoadBalancer"
-echo "> java -cp .:target/testJar-0.7.0-SNAPSHOT-jar-with-dependencies.jar org.thingml.loadbalancer.LoadBalancer" >> $log_file
-java -cp .:target/testJar-0.7.0-SNAPSHOT-jar-with-dependencies.jar org.thingml.loadbalancer.LoadBalancer >> $log_file 2>&1
+java -cp .:target/testJar-0.7.0-SNAPSHOT-jar-with-dependencies.jar org.thingml.loadbalancer.LoadBalancer
 
 echo "> ./dispatch.sh"
-echo "> ./dispatch.sh" >> $log_file
-./dispatch.sh >> $log_file 2>&1
+./dispatch.sh
 
 echo "> DONE"
-echo "> DONE" >> $log_file
 
 
